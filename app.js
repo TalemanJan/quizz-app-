7eb7765973c3c947759d749bf784a2a8c7213e1e
talemanjan <talemanaptech@gmail.com>
2023-03-29 16:56:21 +0500

    added js

--- app.js
+++ app.js
@@ -26,4 +26,22 @@ for(i = 0; i < questions.length; i++) {
 	document.getElementById('question' + [i]).textContent = q
 }
 
+function testResults() {
+	var correct = 0;
+	var incorrect = 0;
+	for(i = 0; i < questions.length; i++) {	
+		var answer = questions[i].answer
+		var guess = document.getElementById('answer' + [i]).value
+		var questionSpot = document.getElementById('question' + [i])
+		if(answer == guess) {
+			questionSpot.className = 'correct'
+			correct++
+		} else {
+			questionSpot.className = 'incorrect'
+			incorrect++
+		}
+	}
+	document.getElementById('correct').textContent = correct
+	document.getElementById('incorrect').textContent = incorrect
+}
 
